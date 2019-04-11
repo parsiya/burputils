9e500a12c1283f6e1451fad30bf0f82b999e2a21
parsiya <parsiya@gmail.com>
2019-04-10 18:33:55 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Burp Utils
+A work-in-progress collection of utilities for creating Burp extensions in
+ Python. **The API is very much subject to change.**
+
+## Usage
+1. Add it as a Python Burp module and use `from burputils import *`.
+    For more info see:
+    https://parsiya.net/blog/2018-12-19-python-utility-modules-for-burp-extensions/
+2. Copy the file to the same path as your extension and use `from burputils import *`.
+    * The second file does not have to be loaded in Burp, it just needs to be in the same path.
+3. Copy/paste used code into your extension.
+
+### Which Option Should I Use?
+I suggest option 1 for development and 2 for release.
+
+If you are only using a few functions, you can copy/paste them at the end of
+ your extension file and avoid having two files.
+
+### Why Do You Use IExtensionHelpers During Construction?
+Burp only allows you to get an instance of that class through callbacks.
+
+Inside your extension, you can either use it directly by setting it manually 
+ or use helpers via `burpUtilsObject.burpHelper`:
+* `utils.burpHelper.buildHttpMessage`
+
+### Burp-Exceptions
+The helper library does not need it yet but you should use it for extension
+ development:
+
+* https://github.com/securityMB/burp-exceptions
+
+## Docs
+To be created.
+
+See [test-extension.py](test-extension.py) for an example. It adds some headers
+ to responses before they hit HTTP History.
+
+## I Found a Bug!
+Bugs in my code? Never!!1! Please make an issue.
+
+## License
+Now that is a can of worms. GPLv3, see [LICENSE](LICENSE) for details.
---
+++ test-extension.py
@@ -0,0 +1,138 @@
+# Burp extension to test my helper modules.
+# it creates a listener and prints information about incoming requests.
+
+# support for burp-exceptions - see https://github.com/securityMB/burp-exceptions
+try:
+    from exceptions_fix import FixBurpExceptions
+    import sys
+except ImportError:
+    pass
+
+from burp import IBurpExtender
+from burp import IHttpListener
+from burputils import BurpUtils
+
+class BurpExtender(IBurpExtender, IHttpListener):
+
+    #
+    # implement IBurpExtender
+    #
+    
+    def	registerExtenderCallbacks(self, callbacks):
+        # obtain an extension helpers object
+        # self.utilss = callbacks.getHelpers()
+        self.utils = BurpUtils(callbacks.getHelpers())
+
+        # enable for burp-exceptions
+        sys.stdout = callbacks.getStdout()
+        
+        # set our extension name
+        callbacks.setExtensionName("Test Helpers")
+        
+        # register an HTTP listener
+        callbacks.registerHttpListener(self)
+
+    #
+    # implement IHttpListener
+    #
+    
+    def processHttpMessage(self, toolFlag, messageIsRequest, messageInfo):
+        
+        # log for requests
+        if messageIsRequest:         
+            # print "Got request"
+            # # print "Request info from raw bytes"
+            # # # process request
+            # # # reqInfo = self.utils.analyzeRequest(messageInfo.getHttpService(), messageInfo.getRequest())
+            # # # reqInfo = self.utils.analyzeRequest(messageInfo)
+            # # reqInfoFromBytes = self.utils.getInfoFromBytes(messageIsRequest, messageInfo.getRequest())
+
+            # # # get method
+            # # print "Method:", reqInfoFromBytes.getMethod()
+
+            # # # get URL
+            # # # this will return an error, use helper.getInfo instead which uses IRequestResponse
+            # # # print "URL:", reqInfoFromBytes.getUrl()
+
+            # # # get headers
+            # # print "Headers:", reqInfoFromBytes.getHeaders()
+
+            # print "Request info from RequestResponse"
+            # # test getInfo
+            # reqInfoFromRequestResponse = self.utils.getInfo(messageIsRequest, messageInfo)
+
+            # # get method
+            # print "Method:", reqInfoFromRequestResponse.getMethod()
+
+            # # get URL
+            # print "URL:", reqInfoFromRequestResponse.getUrl()
+
+            # # get headers
+            # print "Headers:", reqInfoFromRequestResponse.getHeaders()
+
+            # hdr = self.utils.getHeaders(reqInfoFromRequestResponse)
+
+            # # print "Host:", hdr.get("Host")[0]
+            # # print "Cache-Control:", hdr.get("Cache-Control")[0]
+
+            # print "headers recreated"
+            # exported = hdr.exportRaw()
+            # print exported
+            
+            return
+
+        # if we got here, we have a response
+
+        print "Got response"
+
+        # get response info
+        responseInfo = self.utils.getInfo(messageIsRequest, messageInfo)
+        
+        # get headers
+        responseHeaders = responseInfo.getHeaders()
+        print "Response headers"
+        print responseHeaders
+
+        # get headers using utils
+        utilHeaders = self.utils.getHeaders(responseInfo)
+
+        # print util headers
+        print "response headers recreated"
+        respHeaderFromUtils = utilHeaders.exportRaw()
+        print respHeaderFromUtils
+
+        # add something
+        utilHeaders.add("customheader", "customvalue1")
+        utilHeaders.add("customheader", "customvalue2")
+        utilHeaders.add("customheader", "customvalue3")
+
+        # remove `Vary: Accept-Encoding`
+        utilHeaders.remove("Vary")
+
+        # remove `Content-Type` and add our own
+        utilHeaders.overwrite("Content-Type", "Custom content type")
+
+        # print util headers
+        print "response headers recreated after modification"
+        respHeaderFromUtils = utilHeaders.exportRaw()
+        print respHeaderFromUtils
+
+        # put everything back together
+        bodyBytes = self.utils.getBody(messageIsRequest, messageInfo)
+        # build message
+        modifiedmsg = self.utils.burpHelper.buildHttpMessage(respHeaderFromUtils, bodyBytes)
+
+        # set modified message response
+        messageInfo.setResponse(modifiedmsg)
+
+        # this should be reflected in response tab
+
+        # done
+        print "--------"
+        return
+
+# support for burp-exceptions
+try:
+    FixBurpExceptions()
+except:
+    pass
